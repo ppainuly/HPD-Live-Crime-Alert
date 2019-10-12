e69377dc848191515cda74a73096f50d3f5a26cd
Niesha Kovich <nkovich1@hotmail.com>
2019-10-12 13:50:09 -0500

    commit

--- NDK/static/js/crimemap.js
+++ NDK/static/js/crimemap.js
@@ -1,4 +1,28 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
 
+// Add a tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// An array containing each city's name, location, and population
+var cities = [{
+  location: [40.7128, -74.0059],
+  name: "New York",
+  population: "8,550,405"
+},
+{
+  location: [41.8781, -87.6298],
+  name: "Chicago",
+  population: "2,720,546"
+},
 {
   location: [29.7604, -95.3698],
   name: "Houston",
