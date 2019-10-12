a3415a0de9174becd5ca047cdec152453fa2657c
Niesha Kovich <nkovich1@hotmail.com>
2019-10-12 15:09:20 -0500

    Marker Cluster Crime Map

--- NDK/static/js/crimemap.js
+++ NDK/static/js/crimemap.js
@@ -1,7 +1,7 @@
 // Create a map object
-var myMap = L.map("map", {
-  center: [37.09, -95.71],
-  zoom: 5
+var crimeMap = L.map("map", {
+  center: [29.710, -95.376],
+  zoom: 10
 });
 
 // Add a tile layer
@@ -10,90 +10,47 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   maxZoom: 18,
   id: "mapbox.streets",
   accessToken: API_KEY
-}).addTo(myMap);
+}).addTo(crimeMap);
 
-// An array containing each city's name, location, and population
-var cities = [{
-  location: [40.7128, -74.0059],
-  name: "New York",
-  population: "8,550,405"
-},
-{
-  location: [41.8781, -87.6298],
-  name: "Chicago",
-  population: "2,720,546"
-},
-{
-  location: [29.7604, -95.3698],
-  name: "Houston",
-  population: "2,296,224"
-},
-{
-  location: [34.0522, -118.2437],
-  name: "Los Angeles",
-  population: "3,971,883"
-},
-{
-  location: [41.2524, -95.9980],
-  name: "Omaha",
-  population: "446,599"
-},
-{
-  location: [46.8772, -96.7898],
-  name: "Fargo",
-  population: "245,571"
-}
-];
+// Store API query variables
+var baseURL = "https://moto.data.socrata.com/resource/p6kq-vsa3.json";
+// var date = "$where=incident_datetime between'2018-01-01T12:00:00' and '2019-10-10T14:00:00'";
+// var incident = "$where=incident_type_primary"
+// var limit = "&$limit=100";
+  
+  
+  // Assemble API query URL
+// var url = baseURL + date + limit;
+// console.log(url)
+  
+  // Grab the data with d3
+d3.json(baseURL, response => {
+  
+    // Create a new marker cluster group
+  let markers = L.markerClusterGroup();
+  
+console.log(response)
+console.log(response.length)
 
-// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
-for (var i = 0; i < cities.length; i++) {
-  var city = cities[i];
-  L.marker(city.location)
-    .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
-    .addTo(myMap);
-}
-
-//   // Grabbing our GeoJSON data..
-// d3.json(link, function(data) {
-//   // Creating a GeoJSON layer with the retrieved data
-//   L.geoJson(data).addTo(map);
-// });
-
+    // Loop through data
+  for (var i = 0; i <= 100; i++) {
   
-  // // Store API query variables
-  // var baseURL = "https://moto.data.socrata.com/resource/p6kq-vsa3.geojson";
-  // var date = "$where=incident_datetime between'2018-01-01T12:00:00' and '2019-10-10T14:00:00'";
-  // var limit = "&$limit=1000";
+      // Set the data location property to a variable
+    var location = response[i].location;
   
+      // Check for location property
+    if (location) {
   
-  // // Assemble API query URL
-  // var url = baseURL + date + limit;
+        // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].incident_type_primary));
+      }
   
-  // // Grab the data with d3
-  // d3.json(url, response => {
+    }
   
-  //   // Create a new marker cluster group
-  //   let markers = L.markerClusterGroup();
+    // Add our marker cluster layer to the map
+  crimeMap.addLayer(markers);
   
-  //   // Loop through data
-  //   for (var i = 0; i < response.length; i++) {
-  
-  //     // Set the data location property to a variable
-  //     var location = response[i].location;
-  
-  //     // Check for location property
-  //     if (location) {
-  
-  //       // Add a new marker to the cluster group and bind a pop-up
-  //       markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-  //         .bindPopup(response[i].descriptor));
-  //     }
-  
-  //   }
-  
-  //   // Add our marker cluster layer to the map
-  //   crimeMap.addLayer(markers);
-  
-  // });
+  });
   
   
